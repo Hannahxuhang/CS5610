d73023436297c0136d459abb3b0f9cbe17a17e2a
Xu Hang <438034612@qq.com>
2018-02-04 16:35:42 -0800

    add express
    add express.js

--- assignment2/src/server.js
+++ assignment2/src/server.js
@@ -1,2 +1,14 @@
-const port = process.env.PORT || '3100';
-app.set('port', port);
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+
+// PathLocationStrategy
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
+console.log('Console listening!');
